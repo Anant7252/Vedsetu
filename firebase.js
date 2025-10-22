0ff01f910f21e58b3cc7d705023efca5dced4605
Anant7252 <125685873+Anant7252@users.noreply.github.com>
2025-10-22 22:49:39 +0530

    Clear Firebase config values for security
    Removed sensitive Firebase configuration details.

--- firebase.js
+++ firebase.js
@@ -4,14 +4,15 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore'; 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC1EoHW8jRvPrVAIOi3KU7Pl45kGfitkxM",
-  authDomain: "vedsetu-70a6b.firebaseapp.com",
-  projectId: "vedsetu-70a6b",
-  storageBucket: "vedsetu-70a6b.firebasestorage.app",
-  messagingSenderId: "428645528682",
-  appId: "1:428645528682:web:ad33cf873a070e8e908613"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app); 
+
